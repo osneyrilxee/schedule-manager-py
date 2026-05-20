69633c69555a9ff3cd638c9aa13916e6d83f8059
rilxee <rilxee.contact.pro@gmail.com>
2026-05-21 02:19:58 +0630

    firgure out env

--- main.py
+++ main.py
@@ -1,46 +1,28 @@
-#!/usr/bin/env python3
-from textual.app import App, ComposeResult, RenderResult
-from textual.containers import Container
-from textual.widget import Widget
+from textual.app import App, ComposeResult
+from textual.screen import Screen
+from textual.widgets import Placeholder
 
 
-class Hello(Widget):
-
-    def render(self) -> RenderResult:
-        return "_________________________________________\n" \
-        "|                                       |\n" \
-        "|                                       |\n" \
-        "|                                       |\n" \
-        "|            Hi dear user!              |\n" \
-        "|                                       |\n" \
-        "|                                       |\n" \
-        "|_______________________________________|"
-
-class Notes(Widget):
-
-    def render(self) -> RenderResult:
-        return "__________________Notes_________________\n" \
-        "| You can add some notes here           |\n" \
-        "|                                       |\n" \
-        "|                                       |\n" \
-        "|                                       |\n" \
-        "|                                       |\n" \
-        "|_______________________________________|"
+class Header(Placeholder):  
+    pass
 
 
-class ScheduleManagerApp(App):
-    CSS_PATH = "main.tcss"
-
-    BINDINGS = [
-        ("q", "quit", "Quit the app"),
-    ]
+class Footer(Placeholder):  
+    pass
 
 
+class TweetScreen(Screen):
     def compose(self) -> ComposeResult:
-        yield Hello()
-        yield Notes()
+        yield Header(id="Header")  
+        yield Footer(id="Footer")  
 
+class Footer(Placeholder):
+    pass
+
+class ManagerToolApp(App):
+    def on_mount(self) -> None:
+        self.push_screen(TweetScreen())
 
 if __name__ == "__main__":
-    app = ScheduleManagerApp()
+    app = ManagerToolApp()
     app.run()
